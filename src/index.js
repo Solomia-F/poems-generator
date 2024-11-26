d3f84f230eff605cae7bb8a4cba766203d61d071
Solomia-F <solomiia.fedyshyn@gmail.com>
2024-11-26 18:00:31 +0000

    Added styling to the project, added basic form handling, added AI API integration

---
+++ src/index.js
@@ -0,0 +1,27 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: " ",
+    delay: 50,
+  });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "da86o89f290fa6e48t8fe94b9553bb0f";
+  let context =
+    "Your mission is to generate a haiku. Please, separate each line with a <br />. Make sure to follow user instructions";
+  let prompt = `User instructions: Generate a haiku about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(prompt);
+  console.log(context);
+
+  axios.get(apiUrl).then(displayPoem);
+}
+
+let formElement = document.querySelector("#poem-generator-form");
+formElement.addEventListener("submit", generatePoem);
